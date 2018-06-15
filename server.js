66ec2fa206bf74c35e58d09fbbc233b04a3ed7e9
Mauro Velazquez <maurovelazquez1@gmail.com>
2018-06-15 08:38:30 -0300

    Fix issues with .json files contents

--- server.js
+++ server.js
@@ -10,13 +10,14 @@ app.get('/PLCRVoting.json', sendFileContent)
 app.get('/HumanStandardToken.json', sendFileContent)
 
 function sendFileContent(req, res) {
-  const data = fs.readFile(`./contracts${req.route.path}`, 'utf8', (err, data) => {
+  fs.readFile(`./contracts${req.route.path}`, 'utf8', (err, data) => {
     if (err) {
       throw new Error(err)
     }
+    const jsonData = JSON.parse(data)
     res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-    res.json(data)
+    res.json(jsonData)
   })
 }
 
